7bae814b7346913031be4b40c0de50229df3dce4
Jorge Reyes <rjorge99@hotmail.com>
2023-12-23 18:23:28 -0600

    se agrega config para la laptop para habilitar/deshabilitar el teclado

---
+++ keyboard/keyboard.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+Icon="/home/rjorge99/keyboard/keyboard_enabled.png"
+Icoff="/home/rjorge99/keyboard/keyboard_disabled.png"
+fconfig=".keyboard" 
+id=12
+
+if [ ! -f $fconfig ]; then
+  echo "Creating config file"
+  echo "enabled" > $fconfig
+  var="enabled"
+else
+  read -r var< $fconfig
+  echo "keyboard is : $var"
+fi
+
+if [ "$var" = "disabled" ]; then
+  notify-send -i $Icon "Enabling keyboard..." \ "ON - Keyboard connected !";
+  echo "enable keyboard..."
+  xinput enable $id
+  echo "enabled" > $fconfig
+elif [ "$var" = "enabled" ]; then
+  notify-send -i $Icoff "Disabling Keyboard" \ "OFF - Keyboard disconnected";
+  echo "disable keyboard"
+  xinput disable $id
+  echo 'disabled' > $fconfig
+fi
