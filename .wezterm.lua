753fad043f54858a736629352b74634593edf615
Jorge Reyes <rjorge99@hotmail.com>
2023-12-27 20:32:08 -0600

    ctrl + mouse scroll changes font size

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,16 +1,11 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
-
--- This table will hold the configuration.
+local act = wezterm.action
 local config = {}
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
 if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
--- config.default_domain = "WSL:Ubuntu" -- use for windows wsl
 config.enable_tab_bar = false -- enable/disable tabs
 config.font = wezterm.font_with_fallback({ -- fonts
 	"JetBrainsMono Nerd Font",
@@ -23,5 +18,33 @@ config.font_size = 16 -- font size
 config.window_background_opacity = 0.95 -- opacity
 config.color_scheme = "Catppuccin Mocha" -- color scheme
 config.window_decorations = "RESIZE" -- enable/disable window bar
+
+config.mouse_bindings = {
+	-- Bind 'Up' event of CTRL-Click to open hyperlinks
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "CTRL",
+		action = act.OpenLinkAtMouseCursor,
+	},
+	-- Disable the 'Down' event of CTRL-Click to avoid weird program behaviors
+	{
+		event = { Down = { streak = 1, button = "Left" } },
+		mods = "CTRL",
+		action = act.Nop,
+	},
+	-- Scrolling up while holding CTRL increases the font size
+	{
+		event = { Down = { streak = 1, button = { WheelUp = 1 } } },
+		mods = "CTRL",
+		action = act.IncreaseFontSize,
+	},
+
+	-- Scrolling down while holding CTRL decreases the font size
+	{
+		event = { Down = { streak = 1, button = { WheelDown = 1 } } },
+		mods = "CTRL",
+		action = act.DecreaseFontSize,
+	},
+}
 
 return config
