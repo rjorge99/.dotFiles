bd030b17936a8f59e1981bba63299cba00fadbb3
Jorge Reyes <rjorge99@hotmail.com>
2023-12-29 21:07:50 -0600

    Se activa picom en i3

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,7 +7,7 @@ end
 
 config.enable_tab_bar = false -- enable/disable tabs
 config.font = wezterm.font_with_fallback({ -- fonts
-	"SFMono Nerd Font",
+	-- "SFMono Nerd Font",
 	-- "JetBrainsMono Nerd Font",
 	-- "Hack FC Ligatured",
 	"FiraCode Nerd Font",
@@ -15,7 +15,7 @@ config.font = wezterm.font_with_fallback({ -- fonts
 	-- "JetBrains Mono",
 })
 config.font_size = 17 -- font size
--- config.window_background_opacity = 0.95 -- opacity
+config.window_background_opacity = 0.95 -- opacity
 config.color_scheme = "Catppuccin Mocha" -- color scheme
 -- config.window_decorations = "RESIZE" -- enable/disable window bar
 -- config.window_background_image = "/home/rjorge99/wallpapers/groot.jpg"
