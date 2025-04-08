460e3787f4df01879a6fac7614f31d03ad798930
rjorge99 <rjorge99@hotmail.com>
2025-04-08 11:18:48 -0600

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -9,24 +9,21 @@ end
 --config.default_prog = { 'pwsh.exe', '-NoLogo' }
 --config.default_domain = 'WSL:Ubuntu'
 
-config.enable_tab_bar = false -- enable/disable tabs
 config.font = wezterm.font_with_fallback({ -- fonts
-	-- "SFMono Nerd Font",
-	-- "JetBrainsMono Nerd Font",
-	-- "Hack FC Ligatured",
-	"UbuntuMono Nerd Font Mono",
-	-- "JetBrainsMono Nerd Font",
-	-- "Hack FC Ligatured",
-	-- "FiraCode Nerd Font",
-	-- "LigaMonaco Nerd Font",
-	-- "JetBrains Mono",
+	'MesloLGS Nerd Font Mono',
+	'Hack FC Ligatured'
+	"UbuntuMono Nerd Font Mono"	
 })
-config.font_size = 17 -- font size
-config.window_background_opacity = 0.95 -- opacity
-config.color_scheme = "Catppuccin Mocha" -- color scheme
-config.line_height = 1.1 -- line height
+
+config.enable_tab_bar = false 			-- enable/disable tabs
+config.font_size = 17 				-- font size
+config.window_background_opacity = 0.95 	-- opacity
+config.color_scheme = "Catppuccin Mocha" 	-- color scheme
+config.line_height = 1.1 			-- line height
+config.window_close_confirmation = "NeverPrompt"	-- No prompt on exit
+
 -- config.window_decorations = "RESIZE" -- enable/disable window bar
 -- config.window_background_image = "/home/rjorge99/wallpapers/groot.jpg"
-config.window_close_confirmation = "NeverPrompt"
+
 
 return config
